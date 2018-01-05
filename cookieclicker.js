13df2daff0c86cb5eb0b961e1332e0dee53b4f10
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-05 09:44:13 +0200

    uses dir, unminified beautified

--- cookieclicker.js
+++ cookieclicker.js
@@ -1,48 +1,16 @@
-// ==Bookmarklet==
-// @name Auto Cookie Clicker
-// @author coolreader18
-// @script https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @script https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
-// @script https://cdn.jsdelivr.net/npm/js-cookie/src/js.cookie.min.js
-// ==/Bookmarklet==
-
 var updateCookie = () => {
   Cookies.set("AutoCookieOptions", this.options, {
     expires: 365
   });
-}
+};
 
 var openOptions = () => {
-  var opts = this.options,
-    optionsWindow = openOptWin(),
-    optDoc = optionsWindow.document,
-    $options = $(optDoc.body);
+  var opts = this.options, optionsWindow = openOptWin(), optDoc = optionsWindow.document, $options = $(optDoc.body);
   $(optionsWindow).focusout(e => {
-    e.target.close()
-  })
+    e.target.close();
+  });
   optDoc.title = "AutoCookie Options";
-  $options.append("<h1>AutoCookie Options</h1>")
-    .append("<h2>Auto Clicking Options</h2>")
-    .append($("<div>")
-      .append($("<p>")
-        .append($("<input type='checkbox' id='bigCookie'>").prop("checked", opts.bigCookie))
-        .append("<label for='bigCookie'>Big Cookie</label>")
-        .append("<br>")
-        .append($("<label for='cps'>Clicks per second:</label>").css("padding-right", "2px"))
-        .append($("<input id='cps' type='number' min=100 step=100>").css("width", "40px").val(opts.cps)))
-      .append($("<input type='checkbox' id='goldenCookie'>")
-        .prop("checked", opts.goldenCookie))
-      .append("<label for='goldenCookie'>Golden Cookie</label>"))
-    .append("<h2>Auto Buying Options</h2>")
-    .append($("<div>").css("width", "100%")
-      .append($("<div>")
-        .append("<h3>Buying</h3>")
-        .append($("<ol id='buying' class='products'>").data("items", opts.buying)))
-      .append($("<div>")
-        .append("<h3>Not Buying</h3>")
-        .append($("<ol id='notBuying' class='products'>").data("items", opts.notBuying))))
-    .append("<button onclick='window.close()'>Done</button>");
-
+  $options.append("<h1>AutoCookie Options</h1>").append("<h2>Auto Clicking Options</h2>").append($("<div>").append($("<p>").append($("<input type='checkbox' id='bigCookie'>").prop("checked", opts.bigCookie)).append("<label for='bigCookie'>Big Cookie</label>").append("<br>").append($("<label for='cps'>Clicks per second:</label>").css("padding-right", "2px")).append($("<input id='cps' type='number' min=100 step=100>").css("width", "40px").val(opts.cps))).append($("<input type='checkbox' id='goldenCookie'>").prop("checked", opts.goldenCookie)).append("<label for='goldenCookie'>Golden Cookie</label>")).append("<h2>Auto Buying Options</h2>").append($("<div>").css("width", "100%").append($("<div>").append("<h3>Buying</h3>").append($("<ol id='buying' class='products'>").data("items", opts.buying))).append($("<div>").append("<h3>Not Buying</h3>").append($("<ol id='notBuying' class='products'>").data("items", opts.notBuying)))).append("<button onclick='window.close()'>Done</button>");
   $(".products", optDoc).css({
     border: "1px solid #eee",
     width: "142px",
@@ -59,8 +27,8 @@ var openOptions = () => {
         padding: "5px",
         "font-size": "1.2em",
         width: "120px"
-      }))
-    })
+      }));
+    });
   }).sortable({
     connectWith: ".products",
     update: function() {
@@ -68,8 +36,8 @@ var openOptions = () => {
         var cur = opts[i ? "notBuying" : "buying"] = [];
         $(list).children().each(function(i, li) {
           cur.push($(li).data("product"));
-        })
-      })
+        });
+      });
       updateCookie();
     }
   }).disableSelection();
@@ -80,12 +48,12 @@ var openOptions = () => {
   $("input[type='checkbox']", optDoc).change(e => {
     opts[e.currentTarget.id] = e.currentTarget.checked;
     updateCookie();
-  })
+  });
   $("#cps", optDoc).change(e => {
     opts.cps = Number(e.currentTarget.value);
     updateCookie();
-  })
-}
+  });
+};
 
 if (this.clicks) {
   openOptions();
@@ -95,7 +63,7 @@ if (this.clicks) {
     cps: 100,
     goldenCookie: true,
     buying: [],
-    notBuying: [{
+    notBuying: [ {
       ind: 0,
       title: "Cursor"
     }, {
@@ -125,7 +93,7 @@ if (this.clicks) {
     }, {
       ind: 9,
       title: "Antimatter condenser"
-    }]
+    } ]
   };
   updateCookie();
   setInterval(() => {
@@ -140,7 +108,6 @@ if (this.clicks) {
     }
     var buy = opts.buying[Symbol.iterator]();
     buyNext();
-
     function buyNext() {
       var curitem = $("#product" + buy.next().value.ind + ".enabled");
       if (curitem.length) {
